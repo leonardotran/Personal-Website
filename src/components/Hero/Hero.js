d6ec421d1db1961e28bf00652f5f6a7e5107cbb2
Quang Lộc <loctran@Quangs-MBP.attlocal.net>
2023-01-30 16:32:57 -0800

    fix about

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -13,8 +13,14 @@ const Hero = (props) => (
           Loc Tran's Portfolio
         </SectionTitle>
         <SectionText> 
-        Full "Snack" Developer with hands-on experience designing, developing, and implementing applications and solutions using a range of technologies and programming languages. 
-      Seeking to leverage broad development experience and hands-on technical expertise in a challenging role as a Full-stack Developer.     
+        I have experience with Python, Java, JavaScript and modern tech stack (React, NodeJs, MongoDB/Firebase). I am a really curious and passionate software engineer. 
+        I never think there is only one solution for a problem and there is no problem without a solution, the matter is how fast and efficiently we can reach a solution. 
+        I am always interested in learning from my own experiment as well as from my colleagues.
+        I love being challenged as well as overcoming challenges. Through them, I can harden my skills at the same time utilized them for the products I am working on. 
+        Whether it is small or big, I put all my heart into the work. 
+        Even though there is no one without mistakes, I learned from them and remind myself to do better and better. 
+        My current goal in my career is to learn as much as I can and train myself to become the best Software Engineer possible.
+     
         </SectionText>
         <Button onClick={() => window.location = "https://docs.google.com/document/d/1EOQSOZ9lq-iys6eJN4B-qjZSBsSMTZqxreIsWX5KViY/edit?usp=sharing"}>My Resume</Button>
       </LeftSection>
