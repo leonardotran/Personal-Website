a5b5f42aa0940e1a27d618f08a1ae4d6978f90a4
leonardotran <leonardotran2108@gmail.com>
2023-12-04 16:30:19 -0800

    change hero

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -14,11 +14,7 @@ const Hero = (props) => (
         </SectionTitle>
         <SectionText> 
         
-          Loc Tran has extensive experience with Python, Java, JavaScript, and a modern tech stack. He is a remarkably curious and passionate software engineer who firmly believes in the diversity of solutions for any problem, embracing challenges as opportunities for skill enhancement.
-
-          In his approach to work, Loc thrives on being challenged and values the learning that comes with overcoming obstacles. He leverages these experiences to fortify his skills while contributing to the development of the products he's engaged with. Regardless of the project's size, Loc dedicates himself wholeheartedly to the task at hand.
-
-          Recognizing the inevitability of mistakes, Loc views them as learning opportunities. He embraces a mindset of continuous improvement, consistently reminding himself to strive for excellence. In his current career journey, Loc is dedicated to absorbing knowledge and honing his abilities, aspiring to evolve into the best possible Software Engineer.
+        Loc Tran possesses expertise in Python, Java, JavaScript, and modern tech stacks. A highly curious and passionate software engineer, he thrives on challenges, viewing them as opportunities to enhance his skills. Loc brings dedication and heart to every project, regardless of its scale. Embracing a growth mindset, he learns from mistakes, consistently aiming for improvement. Loc's current career goal is to maximize learning and evolve into the best Software Engineer possible.
      
         </SectionText>
         <Button onClick={() => window.location = "https://drive.google.com/file/d/1mXM_02yXsEo8HgEu_6LtDBwanpzuRQgq/view?usp=sharing"}>My Resume</Button>
